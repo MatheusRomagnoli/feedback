28cf1491697a677e37b9411a74a5f8c0cf3222a3
Matheus Romagnoli <matheuszothavio@gmail.com>
2025-03-18 08:34:08 -0300

    ADD: adição do banco de dados compartilhado do professor

--- data/conexao.py
+++ data/conexao.py
@@ -1,12 +1,28 @@
+# import mysql.connector
+# class Conexao: 
+#     def criar_conexao():
+#         conexao = mysql.connector.connect(
+#             host = "localhost",
+#             port = 3306,
+#             user = "root",
+#             password = "root",
+#             database = "dbFeedback"
+#     )
+#         return conexao
+    
+
 import mysql.connector
 class Conexao: 
     def criar_conexao():
         conexao = mysql.connector.connect(
-            host = "localhost",
+            host = "10.110.131.22",
             port = 3306,
-            user = "root",
-            password = "root",
-            database = "dbFeedback"
+            user = "3ds",
+            password = "banana",
+            database = "db_Feedback"
     )
         return conexao
     
+    
+
+
